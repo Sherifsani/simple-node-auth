ed790289faa23360eb2d507d77ec9ab076364358
Sherif Sani <sanisherif11@gmail.com>
2025-03-23 15:36:54 +0100

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const connectToDb = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
 const userRoutes = require("./routes/user-routes");
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 connectToDb();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/home", homeRoutes);
